f70d6bbecd4009f926f3ea2bd329fd1fa09a46a5
Eric Zorn <eric.zorn@quinnipiac.edu>
2017-08-07 17:28:06 -0400

    Bare Bones Application Architecture

--- app.js
+++ app.js
@@ -1,11 +1,45 @@
 /* Coded by Eric Zorn */
 
-var budgetController = (function() {
+
+//First Module
+var budgetController = (function () {
 
     var x = 23;
 
     var add = function (a) {
         return x + a;
+    };
+
+    return {
+        publicTest: function (b) {
+            return add(b);
+        }
     }
 
 })();
+
+
+
+
+
+//Second Module
+var UIController = (function () {
+    // Some code
+})();
+
+
+
+
+
+//Third Module
+var controller = (function (budgetCtrl, UICtrl) {
+
+    var z = budgetCtrl.publicTest(5);
+
+    return {
+        anotherPublic: function () {
+            console.log(z);
+        }
+    };
+
+})(budgetController, UIController);

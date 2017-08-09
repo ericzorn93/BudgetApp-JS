b166dade39be49b34570374d845c29bc93c89a6f
Eric Zorn <goldencatch93@gmail.com>
2017-08-09 12:46:14 -0400

    Proper Data Structures and Returns

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ var budgetController = (function () {
         this.description = description;
         this.value = value;
     };
-    
+
 
     var data = {
         allItems: {
@@ -26,7 +26,42 @@ var budgetController = (function () {
         totals: {
             exp: 0,
             inc: 0
-        };
+        }
+    };
+
+    return {
+      addItem: function (type, des, val) {
+          var newitem, ID;
+
+          //[1 2 3 4 5], next ID = 6
+          //[1 2 4 6 8], next ID = 9
+
+          // ID = last ID + 1
+
+          // Create new ID
+          if (data.allItems[type].length > 0) {
+              ID = data.allItems[type][data.allItems[type].length - 1].id + 1;
+          } else {
+              ID = 0;
+          }
+
+          // Create new item based on 'inc' or 'exp' type
+          if (type === 'exp') {
+              newItem = new Expense(ID, des, val);
+          } else if (type === 'inc') {
+              newItem = new Income(ID, des, val);
+          }
+
+          // Push it into our data structure
+          data.allItems[type].push(newItem);
+
+          // Return the new element
+          return newitem;
+      },
+
+        testing: function () {
+            console.log(data);
+        }
     };
 
 })();
@@ -87,10 +122,14 @@ var controller = (function (budgetCtrl, UICtrl) {
 
 
     var ctrlAddItem = function () {
+
+        var input, newItem;
+
         // 1. Get the field input data
-        var input = UIController.getInput();
+        input = UIController.getInput();
 
         // 2. Add the item to the budget controller
+        newItem = budgetCtrl.addItem(input.type, input.description, input.value);
 
         // 3. Add the new item to the UI
 

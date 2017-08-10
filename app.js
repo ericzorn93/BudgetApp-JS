a581ca235d28f7eb5e7eeeac224b3a9d714d7481
Eric Zorn <eric.zorn@quinnipiac.edu>
2017-08-09 23:02:38 -0400

    Updating Budget Controller and Test for NaN or > 0

--- app.js
+++ app.js
@@ -161,6 +161,15 @@ var controller = (function (budgetCtrl, UICtrl) {
     };
 
 
+    var updateBudget = function () {
+        // 1. Calculate the budget
+
+        // 2. Return the Budget
+
+        // 3. Display the budget on the UI
+    };
+
+
 
     var ctrlAddItem = function () {
 
@@ -169,19 +178,19 @@ var controller = (function (budgetCtrl, UICtrl) {
         // 1. Get the field input data
         input = UICtrl.getInput();
 
-        // 2. Add the item to the budget controller
-        newItem = budgetCtrl.addItem(input.type, input.description, input.value);
+        if (input.description !== "" && !isNaN(input.value) && input.value > 0) {
+            // 2. Add the item to the budget controller
+            newItem = budgetCtrl.addItem(input.type, input.description, input.value);
 
-        // 3. Add the new item to the UI
-        UICtrl.addListItem(newItem, input.type);
+            // 3. Add the new item to the UI
+            UICtrl.addListItem(newItem, input.type);
 
-        // 4. Clear the fields
-        UICtrl.clearFields();
+            // 4. Clear the fields
+            UICtrl.clearFields();
 
-        // 5. Calculate the budget
-
-        // 6. Display the budget on the UI
-
+            // 5. Calculate and Update Budget
+            updateBudget();
+        }
     };
 
     return {

b5481cad663dec399fd73a3d20e6c6ae97bfdba8
Eric Zorn <eric.zorn@quinnipiac.edu>
2017-08-11 13:09:13 -0400

    Adding Budget Object for Expenses and Income

--- app.js
+++ app.js
@@ -17,7 +17,15 @@ var budgetController = (function () {
         this.description = description;
         this.value = value;
     };
+    
+    var calculateTotal = function (type) {
+        var sum = 0;
 
+        data.allItems[type].forEach(function (curr) {
+            sum += curr.value;
+        });
+        data.totals[type] = sum;
+    };
 
     var data = {
         allItems: {
@@ -60,7 +68,35 @@ var budgetController = (function () {
 
           // Return the new element
           return newitem;
-      }
+      },
+        calculateBudget: function () {
+            //Calculate total income and expenses
+                calculateTotal('exp');
+                calculateTotal('inc');
+
+            //Calculate the budget: income - expenses
+                data.budget = data.totals.inc - data.totals.exp;
+
+            //Calculate the percentage of income that we spent
+                if (data.totals.inc > 0) {
+                    data.percentage = Math.round((data.totals.exp / data.totals.inc) * 100);
+                } else {
+                    data.percentage = -1;
+                }
+        },
+
+        getBudget: function () {
+          return {
+            budget: data.budget,
+            totalInc: data.totals.inc,
+            totalExp: data.totals.exp,
+            percentage: data.percentage
+          };
+        },
+
+        testing: function () {
+            console.log(data)
+        }
     };
 
 })();
@@ -163,10 +199,11 @@ var controller = (function (budgetCtrl, UICtrl) {
 
     var updateBudget = function () {
         // 1. Calculate the budget
-
+            budgetCtrl.calculateBudget();
         // 2. Return the Budget
-
+            var budget = budgetCtrl.getBudget();
         // 3. Display the budget on the UI
+        console.log(budget);
     };
 
 

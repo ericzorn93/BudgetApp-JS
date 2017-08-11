db8539a6b0d95fd4fa8b8dd7fa635e4796e1fd2d
Eric Zorn <eric.zorn@quinnipiac.edu>
2017-08-11 13:23:47 -0400

    Updating Inc and Exp UI

--- app.js
+++ app.js
@@ -112,7 +112,11 @@ var UIController = (function () {
         inputValue: ".add__value",
         inputBtn: ".add__btn",
         incomeContainer: '.income__list',
-        expensesContainer: '.expenses__list'
+        expensesContainer: '.expenses__list',
+        budgetLabel: '.budget__value',
+        incomeLabel: '.budget__income--value',
+        expenseLabel: '.budget__expenses--value',
+        percentageLabel: '.budget__expenses--percentage'
     };
 
     return {
@@ -165,6 +169,13 @@ var UIController = (function () {
             fieldsArr[0].focus();
         },
 
+        displayBudget: function (obj) {
+            document.querySelector(DOMstrings.budgetLabel).textContent = obj.budget;
+            document.querySelector(DOMstrings.incomeLabel).textContent = obj.totalInc;
+            document.querySelector(DOMstrings.expenseLabel).textContent = obj.totalExp;
+            document.querySelector(DOMstrings.percentageLabel).textContent = obj.percentage;
+        },
+
         getDOMstrings: function () {
             return DOMstrings;
         }
@@ -203,7 +214,7 @@ var controller = (function (budgetCtrl, UICtrl) {
         // 2. Return the Budget
             var budget = budgetCtrl.getBudget();
         // 3. Display the budget on the UI
-        console.log(budget);
+        UICtrl.displayBudget(budget);
     };
 
 
